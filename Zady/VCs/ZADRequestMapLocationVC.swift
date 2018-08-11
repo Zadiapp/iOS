0db02dc150ab568de2060e0ab3d9439eaa84a478
Kmohamed <khaled.elmorabea@gmail.com>
2018-08-11 05:28:26 +0200

    add Marker and show address on label

--- Zady/VCs/ZADRequestMapLocationVC.swift
+++ Zady/VCs/ZADRequestMapLocationVC.swift
@@ -20,7 +20,9 @@ class ZADRequestMapLocationVC: UIViewController {
     @IBOutlet weak var selectedPlaceLabel:UILabel!
     @IBOutlet weak var confirmButton:ZADButton!
     @IBOutlet weak var mapView:GMSMapView!
-    var mapItems:[GMSMarker]?
+    @IBOutlet weak var markerImageView:UIImageView!
+
+    var location:CLLocationCoordinate2D?
     var delegate:ZADRequestMapLocationVCDelegate?
     
     var mapLocationController:ZADMapLocationController = ZADMapLocationController()
@@ -37,6 +39,21 @@ class ZADRequestMapLocationVC: UIViewController {
         setupView()
     }
     
+    func getGreenImage() -> UIImage {
+        let whiteCircle = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let greenCircle = UIBezierPath(ovalIn: CGRect(x: 3, y: 3, width: 14, height: 14))
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 20, height: 20), false, UIScreen.main.scale)
+        UIColor.white.setFill()
+        whiteCircle.fill()
+        ZADColors.buttonGreenColor.setFill()
+        greenCircle.fill()
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+    
     func setupView() {
         self.clearSearchButton.isHidden = true
     }
@@ -48,11 +65,19 @@ class ZADRequestMapLocationVC: UIViewController {
     }
     
     @IBAction func confirmLocation() {
-        
+        if let location = self.location {
+            self.delegate?.didSelectLocation(mapItem: location)
+            close()
+        }
     }
     
     @IBAction func close() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func clearSearch() {
+        let camera = GMSCameraPosition.camera(withLatitude: 30.0444196, longitude: 31.2357116, zoom: 6.0)
+        self.mapView.camera = camera
     }
     
     
@@ -62,11 +87,12 @@ class ZADRequestMapLocationVC: UIViewController {
     }
     
     func applyTheme() {
+        self.markerImageView.image = getGreenImage()
         self.titleLabel.textColor = ZADColors.titleColor
         self.titleLabel.font = UIFont.systemFont(ofSize: 21.0, weight: .regular)
         self.selectedPlaceLabel.textColor = ZADColors.textFieldTextColor
-        self.selectedPlaceLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .regular)
-        
+        self.selectedPlaceLabel.font = UIFont.systemFont(ofSize: 13.0, weight: .regular)
+        self.selectedPlaceLabel.minimumScaleFactor = 0.3
         self.confirmButton.backgroundColor = ZADColors.buttonGreenColor
         self.confirmButton.setTitleColor(UIColor.white, for: .normal)
     }
@@ -104,18 +130,15 @@ class ZADRequestMapLocationVC: UIViewController {
                     if pm.locality != nil {
                         addressString = addressString + pm.locality! + ", "
                     }
-                    if pm.country != nil {
-                        addressString = addressString + pm.country! + ", "
-                    }
-                    if pm.postalCode != nil {
-                        addressString = addressString + pm.postalCode! + " "
-                    }
+
                     DispatchQueue.main.async {
                         self.selectedPlaceLabel.text = addressString
+                        self.clearSearchButton.isHidden = false
                     }
                 }
         })
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -126,6 +149,7 @@ class ZADRequestMapLocationVC: UIViewController {
 extension ZADRequestMapLocationVC: GMSMapViewDelegate {
     func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
         let coordinate = mapView.camera.target
+        self.location = coordinate
         getAddressFromLatLon(pdblLatitude: "\(coordinate.latitude)", withLongitude: "\(coordinate.longitude)")
     }
 }
